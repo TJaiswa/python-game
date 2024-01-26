7c646f744211aac6490be35a35543157d1cc2848
TJaiswa <157770315+TJaiswa@users.noreply.github.com>
2024-01-25 20:41:35 -0500

    Add files via upload

---
+++ game/levels/Start square.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="64x64 spritesheet" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="64x64 spritesheet.png" width="192" height="64"/>
+</tileset>
---
+++ game/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'XXX                                 XX                                          XXX',
+'XXX                                                                             XXX',
+'XXX XX    XXX       XX     XX              XX                       X           XXX',
+'XXX XX P                                         XX      XX     XXXXXX          XXX',
+'XXX XXXX         XX     XX    XX       XX     XXXXX                 X           XXX',
+'XXX XXXX       XX           XX         X       X  X      XXX                    XXX',
+'XXX XX    X  XXXX    XX  XX       XX XXX      X        XX                       XXX',
+'XXX       X  XXXX    XX  XXX               XXX        X                         XXX',
+'XXX    XXXX  XXXXXX  XX  XXXX XX         XX           X                         XXX',
+'XXXXXXXXXXX  XXXXXX  XX  XXXX      XX       XXX       X                   XXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
